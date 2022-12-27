51cc20b2738e16f0ec10015ad435bb48611ca1d7
Pistifeju <juhasz.pisti7253@gmail.com>
2022-12-27 21:53:10 +0100

    Initial Commit

---
+++ WordleClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordleClone
+//
+//  Created by István Juhász on 2022. 12. 27..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
