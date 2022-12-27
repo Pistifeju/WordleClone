6e2038a8e6e0d908db94377d01d3ef04f9e18033
Pistifeju <juhasz.pisti7253@gmail.com>
2022-12-27 22:06:08 +0100

    Initial files

---
+++ WordleClone/Controller/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  WordleClone
+//
+//  Created by István Juhász on 2022. 12. 27..
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupUI()
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.view.backgroundColor = .systemBlue
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
+    // MARK: - Selectors
+}
